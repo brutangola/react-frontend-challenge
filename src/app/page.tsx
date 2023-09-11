a1ee572ed82aa831abb4c4f6770947e836af0c8b
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-09-11 19:49:23 +0100

    style: header done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen bg-gray-50">
+    <main className="w-screen h-screen">
       
     </main>
   )
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,36 +3,39 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className='w-full h-28 bg-slate-200 mx-auto'>
-      <div>
-        <div>
-          <span>You <span>Tube</span></span>
+    <>
+    <header className='w-full justify-center gap-2 h-32 flex flex-col'>
+      <div className='flex xl:w-[1248px] w-full mx-auto items-center justify-between'>
+        <div className='flex flex-col space-y-2'>
+          <span className='text-black font-semibold text-4xl'>You<span className='text-red-500'>Tube</span></span>
           <span>Broadcast Yourself</span>
         </div>
-        <div>
-          <input type="text"/>
-          <button>Search</button>
+        <div className='flex items-center gap-1'>
+          <input type="text" className='px-10 py-2 border-2 rounded-md'/>
+          <button className='border-2 bg-black text-white px-6 py-2 rounded-md'>Search</button>
         </div>
         <div>
           <span>
-            <a href="#">Create Account</a> or <a href="#">sign-in</a>
+            <a href="#" className='text-blue-700 font-semibold'>Create Account</a> or <a href="#" className='text-blue-700 font-semibold'>sign-in</a>
           </span>
         </div>
       </div>
-      <div>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Videos</li>
-            <li>Channels</li>
+      <div className='xl:w-[1248px] w-full mx-auto flex justify-end'>
+        <nav className='flex items-center xl:w-[900px] justify-between'>
+          <ul className='flex gap-4 text-lg'>
+            <li className='cursor-pointer hover:font-semibold'>Home</li>
+            <li className='cursor-pointer hover:font-semibold'>Videos</li>
+            <li className='cursor-pointer hover:font-semibold'>Channels</li>
           </ul>
-          <ul>
-            <li>Subscriptions</li>
-            <li>History</li>
-            <li>Upload</li>
+          <ul className='flex gap-4 text-lg'>
+            <li className='cursor-pointer hover:font-semibold'>Subscriptions</li>
+            <li className='cursor-pointer hover:font-semibold'>History</li>
+            <li className='cursor-pointer hover:font-semibold'>Upload</li>
           </ul>
         </nav>
       </div>
     </header>
+    <div className='w-[1248px] mx-auto border-b-2 border-black'></div>
+    </>
   )
 }
